f90c80b10bcaa8a0c7a0fc993b30b0147d2147cc
HyunJi Lim <66664845+LimHyun-ji@users.noreply.github.com>
2022-05-23 03:20:37 +0900

    [Feat] Recipe

--- Assets/Scripts/RecipeManager.cs
+++ Assets/Scripts/RecipeManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class RecipeManager : MonoBehaviour
 {
     public string menu="";
-    public List<GameObject> prefabFoods;
-    public List<GameObject> recipeFoods;
-    
+    private string[] menuList={"americano",
+                            "latte", 
+                            "caramel",
+                            "choco",
+                            "lemon",
+                            "orange",
+                            "strawberry",
+                            "mango",
+                            "blueberry",
+                            "ham",
+                            "salmon",
+                            "potato",
+                            "toast" };
+    public List<Sprite> recipeImage;  
+    public Image recipe;  
     public bool isOrder;
     void Start()
     {
@@ -23,65 +37,8 @@ public class RecipeManager : MonoBehaviour
     }
     void SetRecipe()
     {
-        recipeFoods.Clear();
-        if(menu=="americano")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[9]);
-        }
-        if(menu =="latte")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-        }
-        if(menu =="caramel")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-            recipeFoods.Add(prefabFoods[2]);
-        }
-        if(menu =="mocha")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-            recipeFoods.Add(prefabFoods[4]);
-        }
-        if(menu =="latte")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-        }
-        if(menu =="latte")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-        }
-        if(menu =="latte")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-        }
-        if(menu =="latte")
-        {
-            recipeFoods.Add(prefabFoods[5]);
-            recipeFoods.Add(prefabFoods[20]);
-        }
-
-        ShowRecipe();
-
-        isOrder=false;
+        int menuNum=System.Array.IndexOf(menuList, menu);
+        recipe.sprite=recipeImage[menuNum];
     }
-    void ShowRecipe()
-    {
-        int layoutX=0;
-        for(int i=0; i<recipeFoods.Count;i++)
-        {   
-            Transform foodTransform= this.gameObject.transform;
-            //oodTransform.position = new Vector3(layoutX, 0,0);
-
-            var food= Instantiate(recipeFoods[i], foodTransform);
-            food.transform.localPosition = new Vector3(layoutX, 0,0);
-            layoutX += 30;
-        }
-    }
+    
 }
