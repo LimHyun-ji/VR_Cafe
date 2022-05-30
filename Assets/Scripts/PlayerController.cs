8a4cec908d4c8acbe61dad63479bbef529242c29
Plammet <y_e_99@ewhain.net>
2022-05-30 19:16:41 +0900

    Revert "settings"
    This reverts commit 3ad01d0066dd61937beea884bb1aa6f82fb63034.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CustomPlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed=5;
     // Start is called before the first frame update
